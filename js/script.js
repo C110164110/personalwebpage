d31459d61053249d0ac770055b4364270a4db4e7
z.y.xu <48086663+C110164110@users.noreply.github.com>
2023-11-07 21:10:38 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -28,4 +28,38 @@ $(document).ready(function(){
     }
 });
 
+const greeting1 = document.getElementById('greeting-1');
+const greetings = ["HI THERE !"];
+let isDeleting = false;
+let txtIndex = 0;
+
+function typeWriter() {
+    const currentGreeting = greetings[0];
+    const speed = isDeleting ? 50 : 120; // 調整打字速度和刪除速度
+
+    if (!isDeleting && txtIndex <= currentGreeting.length) {
+        greeting1.textContent = currentGreeting.substring(0, txtIndex);
+        txtIndex++;
+    } else if (isDeleting && txtIndex >= 0) {
+        greeting1.textContent = currentGreeting.substring(0, txtIndex);
+        txtIndex--;
+    }
+
+    if (txtIndex > currentGreeting.length) {
+        isDeleting = true;
+    }
+
+    if (txtIndex === 0 && isDeleting) {
+        isDeleting = false;
+    }
+
+    setTimeout(typeWriter, speed);
+}
+
+typeWriter();
+
+
+
+
+
 });
